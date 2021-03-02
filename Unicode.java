0e0bb42093f68de16bc035e970369b90564d1f58
Mariya-E <77268535+Mariya-E@users.noreply.github.com>
2021-03-02 20:53:25 +0300

    Delete Unicode.java

--- Unicode.java
+++
@@ -1,9 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        System.out.print("Cимволы и коды:\n");
-        for (int i = 33; i <= 126; i++) {
-            char ch = (char) i;
-            System.out.printf("%03d %3s%n", i, ch);
-        }
-    }
-}
