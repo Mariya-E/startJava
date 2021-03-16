87fa4df8d1da4d6c40b169a0da029869a585d8f4
Mariya-E <erofeeva.m.m.3@gmail.com>
2021-03-16 20:48:25 +0300

    Update WolfTest.java

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -2,10 +2,9 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.name = "Shiry";
-        wolfOne.age = 3;
-        wolfOne.color = "gray";
-        System.out.println("Кличка - " + wolfOne.name + " , возраст - " + wolfOne.age + " , окрас - " + wolfOne.color);
+        wolfOne.setName("Mona");
+        wolfOne.setAge(10);
+        System.out.println("Кличка - " + wolfOne.getName() + " , возраст - " + wolfOne.getAge() + " , окрас - " + wolfOne.getColor());
         wolfOne.run();
         wolfOne.hunt();
     }
